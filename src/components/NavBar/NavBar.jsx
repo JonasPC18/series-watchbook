20e8a86e6e6d1c7a431d5335e1fea00834b48686
JonasPC18 <134112450+JonasPC18@users.noreply.github.com>
2025-08-10 18:39:45 -0300

    UI com bootstrap no navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,53 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
 
 export default function NavBar() {
   return (
-    <nav>
-      <ul>
-      <li><Link to="/">Página Inicial</Link></li>
-      <li><Link to="cadastro">Cadastrar Série</Link></li>
-      <li><Link to="lista">Lista de Séries</Link></li>
-      <li><Link to="sobre">Sobre</Link></li>
-      </ul>
+    <nav className="navbar navbar-expand-md navbar-dark navbar-custom mb-4">
+      <div className="container">
+        
+        <span className="navbar-brand fw-bold text-uppercase d-flex align-items-center">
+          <i className="bi bi-camera-reels me-2"></i> Series Watchbook
+        </span>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#main-navbar"
+          aria-controls="main-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="main-navbar">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink end to="/" className="nav-link">
+                <i className="bi bi-house me-2"></i>Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/sobre" className="nav-link">
+                <i className="bi bi-info-circle me-2"></i>Sobre
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/cadastro" className="nav-link">
+                <i className="bi bi-plus-square me-2"></i>Cadastrar
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/lista" className="nav-link">
+                <i className="bi bi-tv me-2"></i>Lista de Séries
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
+      </div>
     </nav>
-  )
+  );
 }
