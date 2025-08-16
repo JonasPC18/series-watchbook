f94a7c3a91d6211cf16c570bb1f1b8b22ac34e01
JonasPC18 <134112450+JonasPC18@users.noreply.github.com>
2025-08-15 22:29:27 -0300

    thumb adicionada

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -2,7 +2,7 @@ export default function Sobre() {
   return (
     <>
       <h2>Sobre</h2>
-      <p>
+      <p >
         Este é um projeto de gerenciamento de séries assistidas desenvolvido 
         com React para a diciplina Desenvolvimento de Sistemas Frontend. 
       </p>
